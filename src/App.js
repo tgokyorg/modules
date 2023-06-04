b88292d6198b4c8905f2dd92c467dead70e9fd71
tgoky <tgokyicloud6@gmail.com>
2023-06-04 17:10:51 +0100

    api structuring

--- src/App.js
+++ src/App.js
@@ -1,109 +1,123 @@
-import React, { useState} from 'react';
-import './calculatorstyle.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const Calculator = () => {
 
- const [result, setResult]  = useState('');
+const LoginSignUpApp = () => {
 
- const handleClick = (e) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [ productData, setProductData] = useState(null);
 
-  setResult(result.concat(e.target.name));
- };
-
-  const clear = () => {
-    setResult('')
-  }
-
-  const calculate = () => {
+  const handleSignUp = async  () => {
     try {
+      const response = await axios.post('https://dummyjson.com/products/1', {
+        username,
+        password
+      });
+      // assuming the api returns a success message or merge 
 
-      setResult(eval(result).toString());
+      const user = response.data;
+
+      alert('sign up successful');
+      setLoggedIn(true);
     } catch (error) {
-      setResult('Oops Syntax Error');
+      console.error('Error signing up', error);
+      alert('Error signing up, please try again');
     }
-
   };
 
-  return (
 
-    <div className="calculator">
-      <input type="text" value={result} />
+  const handleLogin = async () => {
+    try {
+      const response = await axios.get('https://dummyjson.com/products/1');
+      const apiProductData = response.data;
 
-      <div className="kaypad">
-        <button className="operator" onClick={clear}>
-          AC
-        </button>
-        
+      // check if the title or price matches the API DATA
+      if (
+        apiProductData.thumbnail ===  'https://i.dummyjson.com/data/products/1/thumbnail.jpg' ||
+        apiProductData.description === 'An apple mobile which is nothing like apple'
+      )
+    {
+    setProductData(apiProductData);
+    setLoggedIn(true);
+    alert('Yay you rock! you know the price ');
+  } else {
+    alert('Invalid ! you dont know shit, try again');
+  }
+} catch (error) {
+  console.error('Error Fetching product data', error);
+}
+  };
 
-        <button name="+" onClick={handleClick}>
-          +
-        </button>
-
-        <button name="-" onClick={handleClick}>
-          -
-        </button>
-
-        <button name="*" onClick={handleClick}>
-          X
-        </button>
-
-
-        <button name="/" onClick={handleClick}>
-          /
-        </button>
-
-        <button name="0" onClick={handleClick}>
-          0
-        </button>
-
-        <button name="1" onClick={handleClick}>
-          1
-        </button>
-
-        <button name="2" onClick={handleClick}>
-          2
-        </button>
-
-        <button name="3" onClick={handleClick}>
-          3
-        </button>
-
-        <button name="4" onClick={handleClick}>
-          4
-        </button>
-
-        <button name="5" onClick={handleClick}>
-          5
-        </button>
-
-        <button name="6" onClick={handleClick}>
-          6
-        </button>
-
-        <button name="7" onClick={handleClick}>
-          7
-        </button>
-
-        <button name="8" onClick={handleClick}>
-          8
-        </button>
-
-        <button name="9" onClick={handleClick}>
-          9
-        </button>
-
-        <button name="." onClick={handleClick}>
-          .
-        </button>
-
-        <button className="calculate" onClick={calculate}>
-          =
-        </button>
+  const renderLoginForm = () => (
+    <div>
+      <h2>please login</h2>
+      <input
+      type="text"
+      placeholder="Username"
+      value={username}
+      onChange ={(e) => setUsername(e.target.value)}
+      />
+        <input
+      type="password"
+      placeholder="Password"
+      value={password}
+      onChange ={(e) => setUsername(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
       </div>
-    </div>
-
   );
+
+  const renderSignUpForm = () => (
+    <div>
+      <h2>Sign renderSignUpForm</h2>
+      <input
+      type="text"
+      placeholder="Username"
+      value={username}
+      onChange ={(e) => setUsername(e.target.value)}
+      />
+        <input
+      type="password"
+      placeholder="Password"
+      value={password}
+      onChange ={(e) => setUsername(e.target.value)}
+      />
+      <button onClick={handleSignUp}>Sign Up</button>
+      </div>
+  );
+
+  const renderProductScreen = () => {
+
+    return (
+      <div>
+        <h1>Welcome , {username} ! </h1>
+        <h2> Product Details:</h2>
+        <img src={productData.thumbnail} alt="pictures" />
+        <p>Price: {productData.description}</p>
+
+      </div>
+    );
+  };
+  
+return (
+ <div>    
+  {loggedIn ? (
+    renderProductScreen()
+  ): (
+    <div>
+      {renderLoginForm()}
+      {renderSignUpForm()}
+      </div>
+  )}
+
+ </div>
+);
+
 }
 
 
-export default Calculator;
+
+
+export default LoginSignUpApp;
